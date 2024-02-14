dd9759b81ebe5c35cf0c121bc3c9e90dcd9a0c01
Huy Khac-Quang Nguyen <kqhuynguyen@gmail.com>
2024-02-14 15:07:55 +0700

    Added Send & jQuery autocomplete.

---
+++ ChatParty/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using ChatParty.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatParty.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ChatPartyAuthContext _context;
+
+        public ContactController(ChatPartyAuthContext context)
+        {
+            _context = context;
+        }
+
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Search(string term)
+        {
+            var suggestions = await _context.MessageGroup.Where(
+                mg => mg.Name.Contains(term)
+                ).Take(10).Select(mg => mg.Name).ToListAsync();
+            suggestions.AddRange(
+                await _context.User.Where(
+                    mg => mg.UserName.Contains(term)
+                ).Select(
+                    m => m.UserName
+                ).Take(10).ToListAsync());
+            return Json(suggestions);
+        }
+
+    }
+}
