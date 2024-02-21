70b8f38107f55685932f35ab703ff10e5f87676c
Huy Khac-Quang Nguyen <kqhuynguyen@gmail.com>
2024-02-22 00:25:14 +0700

    Added routes for Channel: Create and AddUser.

--- ChatParty/Controllers/ChannelController.cs
+++ ChatParty/Controllers/ChannelController.cs
@@ -1,4 +1,5 @@
 ﻿using ChatParty.Areas.Identity.Data;
+using ChatParty.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -30,6 +31,42 @@ namespace ChatParty.Controllers
                 return NotFound();
             }
             return View(messageGroup);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name", "Users")] Channel channel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(channel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index), new { id = channel.Id } );
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddUser(string channelId, string userId)
+        {
+            if (ModelState.IsValid)
+            {
+                var channel = await _context.Channel.Include(c => c.Users).Where(c => c.Id == channelId).FirstOrDefaultAsync();
+                if (channel == null)
+                {
+                    return NotFound();
+                }
+                var user = await _context.User.Where(u => u.Id == userId).FirstOrDefaultAsync();
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                channel.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return Ok(user.Id);
+            }
+            return BadRequest();
         }
     }
 }
